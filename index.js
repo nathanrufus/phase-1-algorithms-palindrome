5c3a26195a824d9a936bdf5bff99ed11f1acaa3c
kibet nathan <nathanrufus540@gmail.com>
2023-10-06 17:11:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length; 
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
